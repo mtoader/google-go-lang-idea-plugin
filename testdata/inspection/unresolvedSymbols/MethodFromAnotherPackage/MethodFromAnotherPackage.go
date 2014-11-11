8439ad8ad97c5524ca2b8e7f9953220fc12a6f82
bronze1man <bronze1man@gmail.com>
2014-11-12 03:39:10 +0800

    add more test cases to ensure method from another package works

--- testdata/inspection/unresolvedSymbols/MethodFromAnotherPackage/MethodFromAnotherPackage.go
+++ testdata/inspection/unresolvedSymbols/MethodFromAnotherPackage/MethodFromAnotherPackage.go
@@ -4,9 +4,48 @@ import (
 	"MethodFromAnotherPackage/p1"
 )
 
-func good1() {
+func test1() {
 	t := p1.T1{}
 	t.Good2()
 
-	t./*begin*/NotExist/*end.Unresolved symbol: 'NotExist'*/()
+	t./*begin*/NotExist1/*end.Unresolved symbol: 'NotExist1'*/()
+}
+func test2() {
+	t := make(p1.T2)
+	t.Good3()
+
+	t./*begin*/NotExist2/*end.Unresolved symbol: 'NotExist2'*/()
+}
+
+func test3() {
+	t := p1.GetT3()
+	t.Good4()
+
+	t./*begin*/NotExist3/*end.Unresolved symbol: 'NotExist3'*/()
+}
+func test4() {
+	t := make(p1.T4)
+	t.Good5()
+
+	t./*begin*/NotExist4/*end.Unresolved symbol: 'NotExist4'*/()
+}
+func test5() {
+	t := make(p1.T5,33)
+	t.Good6()
+
+	t./*begin*/NotExist5/*end.Unresolved symbol: 'NotExist5'*/()
+}
+func test6(){
+	t := p1.GetT6()
+	t.Good7()
+
+	t./*begin*/NotExist6/*end.Unresolved symbol: 'NotExist6'*/()
+
+}
+
+func test7(){
+	t := p1.GetT7()
+	t.Good8()
+
+	t./*begin*/NotExist7/*end.Unresolved symbol: 'NotExist7'*/()
 }
--- testdata/inspection/unresolvedSymbols/MethodFromAnotherPackage/p1/p1.go
+++ testdata/inspection/unresolvedSymbols/MethodFromAnotherPackage/p1/p1.go
@@ -5,3 +5,52 @@ type T1 struct{}
 func (t T1) Good2() {
 
 }
+
+type T2 chan int
+
+func (t T2) Good3() {
+
+}
+
+func GetT3()T3{
+	return T3(nil)
+}
+type T3 func()
+
+func (t T3) Good4() {
+
+}
+
+type T4 map[string]string
+
+func (t T4) Good5() {
+
+}
+
+type T5 []string
+
+func (t T5) Good6() {
+
+}
+
+func GetT6()T6{
+	return T6(0)
+}
+
+type T6 int
+func (t T6) Good7() {
+
+}
+
+type T7 interface{
+	Good8()
+}
+
+func GetT7()T7{
+	return t8(0)
+}
+
+type t8 int
+func (t t8) Good8() {
+
+}
