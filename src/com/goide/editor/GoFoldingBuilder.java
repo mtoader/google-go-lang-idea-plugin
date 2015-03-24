31d1bc6932d18ac718525a6ab029fc14598e29a5
Christian Williams <cwilliams@pivotal.io>
2015-03-24 10:52:56 -0700

    Switch back to GoFoldingBuilder from master branch, it has support for anonymous functions et al.

--- src/com/goide/editor/GoFoldingBuilder.java
+++ src/com/goide/editor/GoFoldingBuilder.java
@@ -18,144 +18,98 @@ package com.goide.editor;
 
 import com.goide.GoParserDefinition;
 import com.goide.GoTypes;
-import com.goide.psi.*;
-import com.intellij.codeInsight.folding.CodeFoldingSettings;
 import com.intellij.lang.ASTNode;
-import com.intellij.lang.folding.FoldingBuilderEx;
+import com.intellij.lang.folding.FoldingBuilder;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiWhiteSpace;
-import com.intellij.psi.TokenType;
-import com.intellij.psi.search.PsiElementProcessor;
 import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
-public class GoFoldingBuilder extends FoldingBuilderEx implements DumbAware {
+/**
+ * Created by IntelliJ IDEA.
+ * User: mtoader
+ * Date: Aug 17, 2010
+ * Time: 11:21:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GoFoldingBuilder implements FoldingBuilder, DumbAware, GoTypes {
   @NotNull
-  @Override
-  public FoldingDescriptor[] buildFoldRegions(@NotNull PsiElement root, @NotNull Document document, boolean quick) {
-    if (!(root instanceof GoFile)) return FoldingDescriptor.EMPTY;
-    GoFile file = (GoFile)root;
-    if (!file.isContentsLoaded()) return FoldingDescriptor.EMPTY;
-    final List<FoldingDescriptor> result = ContainerUtil.newArrayList();
+  public FoldingDescriptor[] buildFoldRegions(@NotNull ASTNode node, @NotNull Document document) {
+    List<FoldingDescriptor> descriptors = new ArrayList<FoldingDescriptor>();
+    appendDescriptors(node.getPsi(), document, descriptors);
+    return descriptors.toArray(new FoldingDescriptor[descriptors.size()]);
+  }
 
-    GoImportList importList = ((GoFile)root).getImportList();
-    if (importList != null) {
-      GoImportDeclaration firstImport = ContainerUtil.getFirstItem(importList.getImportDeclarationList());
-      if (firstImport != null) {
-        PsiElement importKeyword = firstImport.getImport();
-        int offset = importKeyword.getTextRange().getEndOffset();
-        int startOffset = importKeyword.getNextSibling() instanceof PsiWhiteSpace ? offset + 1 : offset;
-        TextRange range = TextRange.create(startOffset, importList.getTextRange().getEndOffset());
-        if (!range.isEmpty()) {
-          result.add(new FoldingDescriptor(importList, range));
-        }
+  private void appendDescriptors(PsiElement psi, Document document, List<FoldingDescriptor> descriptors) {
+    ASTNode node = psi.getNode();
+    if (node == null || !isMultiline(psi)) return;
+    IElementType type = node.getElementType();
+
+    if (GoParserDefinition.MULTILINE_COMMENT == type) {
+      descriptors.add(new FoldingDescriptor(node, node.getTextRange()));
+      return;
+    }
+
+    if (TYPE_DECLARATION == type) {
+      addDescriptorStartFromChildNode(descriptors, node, "{");
+      return;
+    }
+
+    if (CONST_DECLARATION == type || VAR_DECLARATION == type || IMPORT_DECLARATION == type) {
+      addDescriptorStartFromChildNode(descriptors, node, "(");
+      return;
+    }
+
+    if (BLOCK == type) {
+      descriptors.add(new FoldingDescriptor(node, node.getTextRange()));
+    }
+
+    PsiElement child = psi.getFirstChild();
+    while (child != null) {
+      appendDescriptors(child, document, descriptors);
+      child = child.getNextSibling();
+    }
+  }
+
+  private void addDescriptorStartFromChildNode(List<FoldingDescriptor> descriptors, ASTNode node, String childText) {
+    ASTNode startNode = findChildOfText(node, childText);
+    if (startNode != null) {
+      int end = node.getStartOffset() + node.getTextLength();
+      descriptors.add(new FoldingDescriptor(node, new TextRange(startNode.getStartOffset(), end)));
+    }
+  }
+
+  private static ASTNode findChildOfText(ASTNode parent, String text) {
+    ASTNode child = parent.getFirstChildNode();
+    while (child != null) {
+      if (child.getText().equals(text)) {
+        break;
       }
-    }
-
-    for (GoFunctionOrMethodDeclaration method : ContainerUtil.concat(file.getMethods(), file.getFunctions())) {
-      GoBlock block = method.getBlock();
-      if (block != null && block.getTextRange().getLength() > 1) result.add(new FoldingDescriptor(block, block.getTextRange()));
-    }
-
-    for (GoTypeSpec type : file.getTypes()) {
-      foldTypes(type.getType(), result);
-    }
-
-    if (!quick) {
-      final Set<PsiElement> processedComments = ContainerUtil.newHashSet();
-      PsiTreeUtil.processElements(file, new PsiElementProcessor() {
-        @Override
-        public boolean execute(@NotNull PsiElement element) {
-          IElementType type = element.getNode().getElementType();
-          if (type == GoParserDefinition.MULTILINE_COMMENT && element.getTextRange().getLength() > 2) {
-            result.add(new FoldingDescriptor(element, element.getTextRange()));
-          }
-          if (type == GoParserDefinition.LINE_COMMENT) {
-            addCommentFolds(element, processedComments, result);
-          }
-          foldTypes(element, result); // folding for inner types
-          return true;
-        }
-      });
-    }
-    return result.toArray(new FoldingDescriptor[result.size()]);
-  }
-
-  private static void foldTypes(@Nullable PsiElement element, List<FoldingDescriptor> result) {
-    if (element instanceof GoStructType) {
-      addTypeBlock(element, ((GoStructType)element).getLbrace(), ((GoStructType)element).getRbrace(), result);
-    }
-    if (element instanceof GoInterfaceType) {
-      addTypeBlock(element, ((GoInterfaceType)element).getLbrace(), ((GoInterfaceType)element).getRbrace(), result);
-    }
-  }
-
-  private static void addTypeBlock(@NotNull PsiElement element,
-                                   @Nullable PsiElement l,
-                                   @Nullable PsiElement r,
-                                   @NotNull List<FoldingDescriptor> result) {
-    if (l != null && r != null) {
-      result.add(new FoldingDescriptor(element, TextRange.create(l.getTextRange().getStartOffset(), r.getTextRange().getEndOffset())));
-    }
-  }
-
-  // com.intellij.codeInsight.folding.impl.JavaFoldingBuilderBase.addCodeBlockFolds()
-  private static void addCommentFolds(@NotNull PsiElement comment, 
-                                      @NotNull Set<PsiElement> processedComments,
-                                      @NotNull List<FoldingDescriptor> foldElements) {
-    if (processedComments.contains(comment)) return;
-
-    PsiElement end = null;
-    for (PsiElement current = comment.getNextSibling(); current != null; current = current.getNextSibling()) {
-      ASTNode node = current.getNode();
-      if (node == null) break;
-      IElementType elementType = node.getElementType();
-      if (elementType == GoParserDefinition.LINE_COMMENT) {
-        end = current;
-        processedComments.add(current);
-        continue;
+      ASTNode sub = findChildOfText(child, text);
+      if (sub != null) {
+        return sub;
       }
-      if (elementType == TokenType.WHITE_SPACE) continue;
-      break;
+      child = child.getTreeNext();
     }
-
-    if (end != null) {
-      foldElements.add(new FoldingDescriptor(comment, new TextRange(comment.getTextRange().getStartOffset(), end.getTextRange().getEndOffset())));
-    }
+    return child;
   }
-  
-  @Nullable
-  @Override
+
   public String getPlaceholderText(@NotNull ASTNode node) {
-    PsiElement psi = node.getPsi();
-    IElementType type = node.getElementType();
-    if (psi instanceof GoBlock || psi instanceof GoStructType || psi instanceof GoInterfaceType) return "{...}";
-    if (psi instanceof GoImportDeclaration) return "...";
-    if (GoParserDefinition.LINE_COMMENT == type) return "/.../";
-    if (GoParserDefinition.MULTILINE_COMMENT == type) return "/*...*/";
-    return null;
+    return "{ ... }";
   }
 
-  @Override
   public boolean isCollapsedByDefault(@NotNull ASTNode node) {
-    IElementType type = node.getElementType();
-    if (type == GoParserDefinition.LINE_COMMENT || type == GoParserDefinition.MULTILINE_COMMENT) {
-      return CodeFoldingSettings.getInstance().COLLAPSE_DOC_COMMENTS;
-    }
-    if (type == GoTypes.BLOCK && CodeFoldingSettings.getInstance().COLLAPSE_METHODS) {
-      ASTNode parent = node.getTreeParent();
-      return parent != null && parent.getPsi() instanceof GoFunctionOrMethodDeclaration;
-    }
-    return CodeFoldingSettings.getInstance().COLLAPSE_IMPORTS && node.getElementType() == GoTypes.IMPORT_LIST;
+    return false;
+  }
+
+  private static boolean isMultiline(PsiElement element) {
+    String text = element.getText();
+    return text.contains("\n") || text.contains("\r") || text.contains("\r\n");
   }
 }
