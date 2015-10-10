255465ca1b50fcaa75c03e239d3625ab4f84ba80
Florin Patan <florinpatan@gmail.com>
2015-10-10 20:00:14 +0200

    Added folding for type and var declarations and composite literals

--- tests/com/goide/editor/GoFoldingBuilderTest.java
+++ tests/com/goide/editor/GoFoldingBuilderTest.java
@@ -34,4 +34,7 @@ public class GoFoldingBuilderTest extends GoCodeInsightFixtureTestCase {
   public void testEmptyImportList()                               { doTest(); }
   public void testImportListWithNewLineAfterKeyword()             { doTest(); }
   public void testImportListWithOnlyThreeSymbolsToFold()          { doTest(); }
+  public void testVarDeclaration()                                { doTest(); }
+  public void testTypeDeclaration()                               { doTest(); }
+  public void testCompositeLiteral()                              { doTest(); }
 }
