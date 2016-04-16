6bbc694a0f708b0180790c01e2f13a543bedb744
Florin Patan <florinpatan@gmail.com>
2016-04-16 15:30:44 +0100

    Add inspection for infinite for loop (fixes #2443)

--- tests/com/goide/inspections/GoHighlightingTest.java
+++ tests/com/goide/inspections/GoHighlightingTest.java
@@ -70,7 +70,8 @@ public class GoHighlightingTest extends GoCodeInsightFixtureTestCase {
       GoUnderscoreUsedAsValueInspection.class,
       GoRangeIterationOnIllegalTypeInspection.class,
       GoUnusedParameterInspection.class,
-      GoInvalidStringOrCharInspection.class
+      GoInvalidStringOrCharInspection.class,
+      GoInfiniteForInspection.class
     );
   }
 
@@ -153,6 +154,7 @@ public class GoHighlightingTest extends GoCodeInsightFixtureTestCase {
   public void testStringSliceWithThirdIndex() { doTest(); }
   public void testSliceWithThirdIndex()       { doTest(); }
   public void testStringInStructSliceWithThirdIndex() { doTest(); }
+  public void testInfiniteFor()               { doTest(); }
 
   public void testAvoidDuplicatedUnusedImportReports() {
     myFixture.addFileToProject("pack1/a.go", "package foo;");
