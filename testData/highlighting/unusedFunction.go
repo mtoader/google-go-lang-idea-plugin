6684cdd3201db55636f4cd2954f4dfa7cdbcf514
Florin Patan <florinpatan@gmail.com>
2016-07-10 21:10:02 +0100

    Added test

--- testData/highlighting/unusedFunction.go
+++ testData/highlighting/unusedFunction.go
@@ -11,6 +11,25 @@ func <warning descr="Unused function 'demo2'">demo2</warning>() {}
 
 func demo3() {}
 
+
+func alpha() {
+	alpha()
+}
+
+func <warning descr="Unused function 'beta'">beta</warning>() {
+	alpha()
+}
+
+
+func bar() {
+	foo()
+}
+
+func foo() {
+	foo()
+}
+
+
 func init() {
 	demo3()
 }
